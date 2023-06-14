2b285d8422be5a2f5a909032c96fe78b7c363aff
smit-skyttus <smit.skyttus@gmail.com>
2023-06-14 15:19:06 +0530

    init1

--- src/Auth/msalConfig.js
+++ src/Auth/msalConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
       clientId: '424528b5-84c5-4fe5-a80b-09cfc380c2d9',
-      redirectUri: 'http://localhost:3000',
+      redirectUri: 'https://react-travel-4o3q2t7ix-rgp3107.vercel.app/',
       authority: 'https://login.microsoftonline.com/36e1c069-10f4-4c1f-b618-b20887b47691',
     },
     cache: {
